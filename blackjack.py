76549c4fe9ea79e116186b307cb18f4ecec6045f
Dylan Marchese <dmarchese@uchicago.edu>
2026-04-30 11:36:58 -0500

    Added action functions and started game loop TUI

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,7 @@
 #blackjack single person game
 from random import shuffle
 
+NUM_DECKS = 6
 CARD_VALUES = {
     2,3,4,5,6,7,8,9,10,"Jack","Queen","King","Ace"
 }
@@ -44,7 +45,79 @@ class Deck():
     def shuffle_it(self):
         shuffle(self.card_order)
 
+    @property
     def draw_card(self):
         card = self.card_order.pop(0)
         self.card_order.append(card)
         return card
+
+
+play_deck = Deck(NUM_DECKS)
+game_loop = True
+
+def start_hand():
+    #bet, one player, one dealer face up, one player, one dealer face down
+    player_cards.append(play_deck.draw_card)
+    dealer_cards.append(play_deck.draw_card) #face up
+    player_cards.append(play_deck.draw_card)
+    dealer_cards.append(play_deck.draw_card) #face down
+
+def hit():
+    card = play_deck.draw_card
+    player_cards.append(card)
+    print(f"You drew the {card}")
+
+def double_down():
+    hit()
+    able_to_hit = False
+
+def stand():
+    pass
+
+def check_card_values(hand: list):
+    total = 0
+    ace = False
+    for card in hand:
+        if card.alt_value is not None:
+            ace = True
+        total += card.value
+    if total > 21 and ace:
+        total = 0
+        for card in hand:
+            if card.alt_value is not None:
+                total += card.alt_value
+            else:
+                total += card.value
+    return total
+        
+        
+
+
+while game_loop:
+    player_cards = []
+    dealer_cards = []
+    able_to_hit = True
+    start_hand()
+    print(f"You get dealt the {player_cards[0]} and the {player_cards[1]}")
+    print(f"The dealer is showing the {dealer_cards[0]}")
+    print("What do you want to do?")
+    need_answer = True
+    while need_answer:
+        x = input('h for hit, s for stand, d for double down, and q for quit')
+        if x not in {'h', 's', 'd', 'q'}:
+            print('Invalid input')
+        elif x == 'h':
+            hit()
+            need_answer = False
+        elif x == 's':
+            stand()
+            need_answer = False
+        elif x == 'd':
+            double_down()
+            need_answer = False
+        elif x == 'q':
+            print('Thanks for playing.')
+            need_answer = False
+            game_loop = False
+    
+        
